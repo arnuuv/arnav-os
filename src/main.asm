08d5b6da146da290f0202a407e2001d9f4a8399d
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-07-30 13:03:14 +0530

    writing an interrupt

--- src/main.asm
+++ src/main.asm
@@ -1,12 +1,52 @@
 org 0x7C00
 bits 16
 
+start:
+  jmp main
+
+; 
+; Prints a string to the screen.
+; Params :
+;     -ds:si points to a string
+;
+;
+puts:
+  ;save reisters we will modify
+  push si
+  push ax
+
+.loop:
+  lodsb       ;loads character into al
+  or al,al    ;
+  jz .done
+  jmp .loop
+
+
+.done:
+  pop ax
+  pop si
+  ret 
+
+
 
 main: 
+  ;setup data segments
+  mov ax,0    ;cant write directly to ds/es
+  mov ds,ax
+  mov es,ax
+
+  ;setup stack
+  mov ss,ax
+  mov sp, 0x7C00   ;stack grows downwards from where loaded in memory
+
+
+
   hlt
 
 .halt:
   jmp .halt
+
+
 
 times 510-($-$$) db 0
 dw 0AA55h
