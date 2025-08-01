912f869f8f13216396f90d10510bcb28cde5edc3
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-08-01 21:40:33 +0530

    reading from disk 1

---
+++ kernel/main.asm
@@ -0,0 +1,67 @@
+org 0x7C00
+bits 16
+
+%define ENDL 0x0D,0x0A
+
+
+start:
+  jmp main
+
+; 
+; Prints a string to the screen.
+; Params :
+;     -ds:si points to a string
+;
+;
+puts:
+  ;save reisters we will modify
+  push si
+  push ax
+
+.loop:
+  lodsb       ;loads character into al
+  or al,al    ;verify if next character is null
+  jz .done
+  
+  mov ah, 0x0e    ;call bios interrupt
+  mov bh,0
+  int 0x10
+  jmp .loop
+
+
+
+.done:
+  pop ax
+  pop si
+  ret 
+
+
+
+main: 
+  ;setup data segments
+  mov ax,0    ;cant write directly to ds/es
+  mov ds,ax
+  mov es,ax
+
+  ;setup stack
+  mov ss,ax
+  mov sp, 0x7C00   ;stack grows downwards from where loaded in memory
+
+  ;print message
+  mov si,msg_hello
+  call puts
+
+
+
+  hlt
+
+.halt:
+  jmp .halt
+
+msg_hello: db 'Hello world',ENDL,0
+
+times 510-($-$$) db 0
+dw 0AA55h
+
+
+
